711b349fa0646e6a7bf3a0194b0261577c46198a
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2022-11-30 21:01:25 +0530

    Add files via upload

--- projectscolor/popup.js
+++ projectscolor/popup.js
@@ -2,43 +2,42 @@ const btn = document.querySelector(".changeColorBtn");
 const colorGrid = document.querySelector(".colorGrid");
 const colorValue = document.querySelector(".colorValue");
 btn.addEventListener("click", async () => {
-    chrome.storage.sync.get("color", ({ color }) => {
-        console.log("color: ", color);
-    });
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.storage.sync.get("color", ({ color }) => {
+    console.log("color: ", color);
+  });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tab.id },
-            function: pickColor,
-        },
-        async (injectionResults) => {
-            const [data] = injectionResults;
-            if (data.result) {
-                const color = data.result.sRGBHex;
-                colorGrid.style.backgroundColor = color;
-                colorValue.innerText = color;
-                try {
-                    await navigator.clipboard.writeText(color);
-                } catch (err){
-                    console.log(err);
-                }
-                console.log(colorGrid);
-            }
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (injectionResults) => {
+      const [data] = injectionResults;
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerText = color;
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (err) {
+          console.log(err);
         }
-    );
+        console.log(colorGrid);
+      }
+    }
+  );
 });
 
 async function pickColor() {
-    try {
-        // Picker
-        const eyeDropper = new EyeDropper();
-        return await eyeDropper.open();
-    } catch (err) {
-        console.log(err);
-    }
-};
+  try {
+    // Picker
+    const eyeDropper = new EyeDropper();
+    return await eyeDropper.open();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 //color picker
-$(document).ready(function(){
-    $("#myColorField").kendoColorPicker();
-});
+
